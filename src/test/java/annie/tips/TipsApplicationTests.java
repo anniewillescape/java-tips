3cac58a3e1972ceb123cf196a4a930a7a4b10352
anniewillescape <meteor.voice@gmail.com>
2023-08-19 12:01:46 +0900

    base project

---
+++ src/test/java/annie/tips/TipsApplicationTests.java
@@ -0,0 +1,13 @@
+package annie.tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
